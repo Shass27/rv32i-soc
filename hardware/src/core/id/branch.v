d6fe5a648f86b421b682b2495709379e3be37b10
Shaswath S <shaswaths@icloud.com>
2026-07-30 20:01:19 +0530

    Improvised branch logic in id stage

--- hardware/src/core/id/branch.v
+++ hardware/src/core/id/branch.v
@@ -1,10 +1,11 @@
 module branch_logic(
     input wire branch,
     input wire alu_zero,
-    output wire target
+    input  wire [31:0] pc,
+    output wire branch_taken,
+    output wire [31:0] target
 );
-    wire branch_target;
-    assign branch_target = alu_zero & branch;
-    assign target = branch_target;
+    assign branch_taken = alu_zero & branch;
+    assign target = branch_taken ? branch_taken + pc : 32'b0;
 
 endmodule

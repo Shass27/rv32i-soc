2601df90aff7608d334142fdcc25b513c40e94d1
kkishorakarthik <klkishorakarthik@gmail.com>
2026-08-02 16:26:02 +0530

    Update inst_mem.v

--- hardware/src/core/if/inst_mem.v
+++ hardware/src/core/if/inst_mem.v
@@ -1,9 +1,9 @@
 module inst_mem(
-      input [31:0] i_addr, output [31:0] instruction
+      input [31:0] i_addr, output [31:0] instr
     );
     reg [31:0] mem [0:1023];
     initial begin
     $readmemh("program.hex",mem);
     end
-    assign instruction = mem[i_addr>>2]; // shift 2 bits to the right as we are using word addressing
+    assign instr = mem[i_addr>>2]; // shift 2 bits to the right as we are using word addressing
 endmodule

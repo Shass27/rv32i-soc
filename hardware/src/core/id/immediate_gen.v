9d198b16ae1a0826cacb654173d0be71d69c400b
Shaswath S <shaswaths@icloud.com>
2026-08-11 16:16:16 +0530

    modified the id stage for milestone 2

--- hardware/src/core/id/immediate_gen.v
+++ hardware/src/core/id/immediate_gen.v
@@ -2,29 +2,27 @@ module imm_gen (
     input wire [31:0] instr,
     output reg [31:0] imm
 );
-    
+
     wire [6:0] opcode  = instr[6:0];
 
-    localparam ADDI  = 7'b0010011; // ADDI
-    localparam LOAD   = 7'b0000011; // LW
-    localparam STORE  = 7'b0100011; // SW
-    localparam BRANCH = 7'b1100011; // BEQ
-    localparam JAL    = 7'b1101111; // JAL
+    localparam IMM  = 7'b0010011; // I-type ALU : ADDI ANDI ORI XORI SLLI SRLI SRAI SLTI SLTIU
+    localparam LOAD    = 7'b0000011; // I-type LOAD : LB LH LW LBU LHU
+    localparam STORE   = 7'b0100011; // S-type STORE : SB SH SW
+    localparam BRANCH  = 7'b1100011; // B-type BRANCH : BEQ BNE BLT BGE BLTU BGEU
+    localparam JAL     = 7'b1101111; // J-type : JAL
 
     always @(*) begin
         case (opcode)
-            ADDI, LOAD:
-                // I-type immediate
-                imm  = {{20{instr[31]}}, instr[31:20]};
-
+            IMM, LOAD:
+                imm = {{20{instr[31]}}, instr[31:20]};
             STORE:
                 // S-type immediate
                 imm = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-            
+
             BRANCH:
                 // SB-type immediate
                 imm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-            
+
             JAL:
                 // UJ-type immediate
                 imm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};

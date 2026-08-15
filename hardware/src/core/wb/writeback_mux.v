48ba41ecc7a4713b55d2dc891509a7f13959b062
namaswi-v <vajjalanamaswi@gmail.com>
2026-08-15 18:04:01 +0530

    update

--- hardware/src/core/wb/writeback_mux.v
+++ hardware/src/core/wb/writeback_mux.v
@@ -1,14 +1,14 @@
 module writeback_mux(
     input  wire MemToReg,
-    input wire jump,
+    input wire jump1,
+    input wire jump2,
     input  wire [31:0] alu_result,
     input  wire [31:0] mem_rdata, //Data Read from memory
     input  wire [31:0] jump_ret_addr, 
     output wire [31:0] wb_data
 );
-
-    assign wb_data = (MemToReg) ? mem_rdata : alu_result;
-    assign wb_data = (jump) ? jump_ret_addr :
+ 
+    assign wb_data = ((jump1) | (jump2)) ? jump_ret_addr :
                      (MemToReg) ? mem_rdata :
                      alu_result;
-endmodule
+endmodule 

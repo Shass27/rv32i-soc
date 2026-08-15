d6536a528e3f16cc7640145d5b045590a5160d1b
kkishorakarthik <klkishorakarthik@gmail.com>
2026-08-15 18:46:21 +0530

    Update program_counter.v

--- hardware/src/core/if/program_counter.v
+++ hardware/src/core/if/program_counter.v
@@ -2,12 +2,13 @@ module program_counter (
     input wire        clk,
     input wire        reset,
     input wire        branch_taken,
-    input wire        jump,       
+    input wire        jump1,
+    input wire        jump2,       
     input wire [31:0] target,   // the branch/jump target address
     output reg  [31:0] pc
 );
     wire pc_sel;
-    assign pc_sel = branch_taken | jump;
+    assign pc_sel = branch_taken | jump1 | jump2;
     always @(posedge clk or posedge reset) begin
         if (reset)
             pc <= 32'b0;
